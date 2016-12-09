e6261b82386770ff7c20e0d9a7a38967b45fa9b6
Cesar Cardenas <cesar.cardam@gmail.com>
2016-12-09 09:31:00 -0600

    ready to be up

--- Python/WordRelation/Python/main.py
+++ Python/WordRelation/Python/main.py
@@ -35,7 +35,7 @@ def linker(s,n):
 def linkerC(s,n,yearInit,yearEnd):
     n=int(n)
     numResults=50
-    #Busca en bing
+    #Busca en corpus
     descriptionsList=websearcher.getResultsDescriptionListCorpus("../Outputs",s,yearInit,yearEnd)
     #Limpia la lista de signos de puntuación y palabras de baja información
     cleanList=cleaner.removeMarksAndLowInfoWordsFromList(descriptionsList,"../Resources/low_info_words.txt")
@@ -50,6 +50,30 @@ def linkerC(s,n,yearInit,yearEnd):
     #Guarda en un archivo de texto las listas de tuplas
     #writer.writeListOfTuples("../Outputs/"+s+str(n)+"gramas.txt",wordsRelationOrdered)
     response = writer.writeDataForD3("../../../Resources/data.json",s,wordsRelationOrdered,100,10)
+
+    return response
+
+def freqData():
+    yearInit=1960
+    yearEnd=2015
+    n=1
+    numResults=50
+    jsonV={}
+    #Busca en corpus
+    for year in range(yearInit,yearEnd)):
+        descriptionsList=websearcher.getResultsDescriptionListCorpus("../Outputs",s,year,year)
+        #Limpia la lista de signos de puntuación
+        cleanList=cleaner.removeMarksFromList(descriptionsList)
+        #Genera los 3gramas en una lista de listas
+        nGramsList=ngramer.makeNGramListFromTextList(cleanList,n)
+        #Obtiene la probabilidad condicional de la palabra dada con cada palabra del texto en una lista de tuplas
+        #Se pasa el valor de lambda de 1 para el smoothing de Laplace, también se pasa el número de resultados
+        #para calcular el número de N-1 gramas
+        freqDic,words=freqcounter.getWordsFreqInNgrams(nGramsList)
+        
+    #Guarda en un archivo de texto las listas de tuplas
+    #writer.writeListOfTuples("../Outputs/"+s+str(n)+"gramas.txt",wordsRelationOrdered)
+    response = writer.writeDataForD3("../../../Resources/data.tsv",s,wordsRelationOrdered,100,10)
 
     return response
 
